0e24e080317bd82023bf3628d2ccf1e667f49f52
nmeinhold <43660864+NateMeinhold@users.noreply.github.com>
2019-03-07 20:10:28 -0800

    worked with becky.

--- script.js
+++ script.js
@@ -1,58 +1,73 @@
 const totalIngredients = {}
 
-document.addEventListener('DOMContentLoaded',function(e){
-  //Makes sure that all HTML is rendered before moving on
-  document.addEventListener('submit', function(e){
-    //stop event bubbling -what happens if you take this out?
-    e.preventDefault();
+document.addEventListener('DOMContentLoaded', function (e) {
+    //Makes sure that all HTML is rendered before moving on
+    document.addEventListener('submit', function (e) {
+        //stop event bubbling -what happens if you take this out?
+        e.preventDefault();
 
-    //what is this?
-   
+        //what is this?
 
-    //get value of input
-    let itemInput = document.querySelector('input[name=item-input]');
-    let itemValue = itemInput.value;
 
-    //add a new list item to list
+        //get value of input
+        let itemInput = document.querySelector('input[name=item-input]');
+        let itemValue = itemInput.value;
 
-    //create new item
-    let newItemEl = document.createElement('li');
-    let newCheckboxEl = document.createElement('input');
-    //setting checkbox to each new item
-    newCheckboxEl.setAttribute("type", "checkbox");
-    newCheckboxEl.value = itemValue;
-    let newSpanEl = document.createElement('span');
-    newSpanEl.innerHTML= itemValue;
-    newItemEl.appendChild(newCheckboxEl);
-    newItemEl.appendChild(newSpanEl);
-    const ingredients = document.querySelector('.ingredients');
-    ingredients.appendChild(newItemEl);
+        //add a new list item to list
 
-    newCheckboxEl.addEventListener ('click', function(event){
-    //alert(this.value);
-    debugger
-    if (this.checked){
-      totalIngredients[this.value] = true;      
-    }
-    else {
-      totalIngredients[this.value] = false;
-    }
-      console.log(totalIngredients);
+        //create new item
+        let newItemEl = document.createElement('li');
+        let newCheckboxEl = document.createElement('input');
+        //setting checkbox to each new item
+        //ewCheckboxEl.setAttribute("type", "checkbox");
+        //newCheckboxEl.value = itemValue;
+        let newSpanEl = document.createElement('span');
+        newSpanEl.innerHTML = itemValue;
+        newSpanEl.setAttribute("data-name", itemValue);
+        //newItemEl.appendChild(newCheckboxEl);
+        newItemEl.appendChild(newSpanEl);
+        const ingredients = document.querySelector('.ingredients');
+        ingredients.appendChild(newItemEl);
+        let iconEl = document.createElement('i');
+        iconEl.innerHTML = "add";
+        newItemEl.appendChild(iconEl);
+        iconEl.addEventListener("click", function (event) {
+            let purchased = document.querySelector("#previous-list-container");
+            let ingredientEl = event.target.previousSibling;
+            let food = ingredientEl.getAttribute("data-name");
+            console.log("testing,", food);
+            let foodList = document.createElement('li');
+            foodList.innerHTML = food;
+            purchased.appendChild(foodList);
+        })
+
+        newCheckboxEl.addEventListener('click', function (event) {
+            //alert(this.value);
+            debugger
+            if (this.checked) {
+                totalIngredients[this.value] = true;
+            } else {
+                totalIngredients[this.value] = false;
+            }
+            console.log(totalIngredients);
+        })
+
+        //newItemEl = document.createElement("INPUT");
+        //newItemEl.setAttribute("type", "checkbox");
+
+        //get a reference to list and append
+        let list = document.querySelector('.ingredients');
+        list.appendChild(newItemEl);
+        itemInput.value = '';
+
+
+
+
     })
-
-    //newItemEl = document.createElement("INPUT");
-    //newItemEl.setAttribute("type", "checkbox");
-
-    //get a reference to list and append
-    let list = document.querySelector('.ingredients');
-    list.appendChild(newItemEl);
-    itemInput.value = '';
-
-  
-
-
-  })
 })
+
+
+
 //becky's example
 //let keys = Object.keys(totalIngredients)
 //for (key of keys){
@@ -66,16 +81,16 @@ document.addEventListener('DOMContentLoaded',function(e){
 //tried with an event listener
 //let keys = Object.keys(totalIngredients)
 //for (key of keys){
-  //if (obj[key]){
-    //console.log(`${obj[key]} is true` )
-    //newCheckboxEl.addEventListener('click', function(event){
-     // let previousListContainer = document.createElement('li');
-     // const previousListContainer = document.querySelector('#previous-list-container');
-    //ingredients.appendChild(totalIngredients);
-    //})
-  //} else {
- // console.log(`${obj[key]} is false`)
- // }
+//if (obj[key]){
+//console.log(`${obj[key]} is true` )
+//newCheckboxEl.addEventListener('click', function(event){
+// let previousListContainer = document.createElement('li');
+// const previousListContainer = document.querySelector('#previous-list-container');
+//ingredients.appendChild(totalIngredients);
+//})
+//} else {
+// console.log(`${obj[key]} is false`)
+// }
 //}
 
 //tried with a boolean
@@ -104,4 +119,4 @@ document.addEventListener('DOMContentLoaded',function(e){
 
 //object keys in js 
 //Object.keys(object2).sort() 
-//could use to do something with object: const totalIngredients 
+//could use to do something with object: const totalIngredients
