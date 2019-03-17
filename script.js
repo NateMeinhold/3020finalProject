c9c0fcf1dc94ec6d8cd47d37f1c92d20eb4a1b86
nmeinhold <43660864+NateMeinhold@users.noreply.github.com>
2019-03-17 16:12:40 -0700

    checking over code again.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const totalIngredients = {}
 document.addEventListener('DOMContentLoaded', function (e) {
     //Makes sure that all HTML is rendered before moving on
     document.addEventListener('submit', function (e) {
-        //stop event bubbling -what happens if you take this out?
+        //stop event bubbling 
         e.preventDefault();
 
         //get value of input
@@ -15,18 +15,20 @@ document.addEventListener('DOMContentLoaded', function (e) {
         //create new item
         let newItemEl = document.createElement('li');
         let newCheckboxEl = document.createElement('input');
-        //setting plus to each new item
         let newSpanEl = document.createElement('span');
         newSpanEl.innerHTML = itemValue;
         newSpanEl.setAttribute("data-name", itemValue);
         newItemEl.appendChild(newSpanEl);
         const ingredients = document.querySelector('.ingredients');
         ingredients.appendChild(newItemEl);
+
+        //adding the (+) to each list item
         let iconEl = document.createElement('i');
         iconEl.classList.add('fas');
         iconEl.classList.add('fa-plus');
         newItemEl.appendChild(iconEl);
-        //add things list item to "purchased" list
+
+        //add listed item to "purchased" list
         iconEl.addEventListener("click", function (event) {
             let purchased = document.querySelector("#previous-list-container");
             let ingredientEl = event.target.previousSibling;
@@ -36,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
             foodList.innerHTML = food;
             purchased.appendChild(foodList);
         })
-
+        //check if checked
         newCheckboxEl.addEventListener('click', function (event) {
             //alert(this.value);
             debugger
@@ -54,9 +56,20 @@ document.addEventListener('DOMContentLoaded', function (e) {
         itemInput.value = '';
 
         //creating the array 
-       // let salad = ['lettuce', 'tomato', 'feta', 'bacon'];
-        salad.addEventListener('click', function(event){
+        let salad = ["lettuce", "tomato", "feta", "bacon"];{
             
+        }
+        //adding array items to list
+        let button = document.querySelector("salad");
+        button.addEventListener('click', function (event) {
+            let newItemEl = document.createElement('li');
+            let newCheckboxEl = document.createElement('input');
+            let newSpanEl = document.createElement('span');
+           newSpanEl.innerHTML = itemValue;
+            newSpanEl.setAttribute("data-name", itemValue);
+            newItemEl.appendChild(newSpanEl);
+            const ingredients = document.querySelector('.ingredients');
+            ingredients.appendChild(newItemEl);
         })
 
     })
@@ -74,20 +87,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
 //  }
 //}
 
-//tried with an event listener
-//let keys = Object.keys(totalIngredients)
-//for (key of keys){
-//if (obj[key]){
-//console.log(`${obj[key]} is true` )
-//newCheckboxEl.addEventListener('click', function(event){
-// let previousListContainer = document.createElement('li');
-// const previousListContainer = document.querySelector('#previous-list-container');
-//ingredients.appendChild(totalIngredients);
-//})
-//} else {
-// console.log(`${obj[key]} is false`)
-// }
-//}
 
 
 //need to research JS for edit/delet funtion
